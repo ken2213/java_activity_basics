ba34381cdc35ccdf0b16dafc7d6fc0b01cc2aa8f
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-06-30 20:44:37 +0800

    Implemented: Passing Data to Another Activity using putExtra

--- app/src/main/java/com/example/activitybasics/SettingsActivity.java
+++ app/src/main/java/com/example/activitybasics/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.activitybasics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class SettingsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent i = getIntent();
+        String message = i.getStringExtra("COOL");
+
+        ((TextView) findViewById(R.id.textView)).setText(message);
     }
 }
